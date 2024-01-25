e0c999d049c7eea9d97d0ff542cf9095001c35af
daochinam <daochinam1081406@gmail.com>
2024-01-26 01:00:36 +0700

    fix css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  content: ["./src/**/*.{html,jsx}", "./node_modules/flowbite/**/*.js"],
   mode: "jit",
   theme: {
     extend: {
